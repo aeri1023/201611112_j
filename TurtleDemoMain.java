c4b48dcd1206bff7d59ab303942469a6c9bee49c
201611112 차애리/201611112_j <go1132@naver.com>
2016-10-16 21:02:05 +0900

    10/12 turtle

---
+++ TurtleDemoMain.java
@@ -0,0 +1,12 @@
+package com.sd.turtle;
+import ch.aplu.turtle.*;
+
+class TurtleDemoMain{
+public static void main(String[] args){
+Turtle t1=new Turtle();
+for(int i=0;i<4;i++){
+t1.fd(50);
+t1.lt(90);
+}
+}
+}
