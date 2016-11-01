5d313c350d7c976187373cc0bd380efe44013ff9
201611112 차애리/201611112_j <go1132@naver.com>
2016-11-02 04:05:14 +0900

    10.31 화씨,섭시 온도변환

---
+++ Temperature.java
@@ -0,0 +1,16 @@
+class Temperature{
+void change(String a, float b){
+    float c;
+    if(a.equals("F")){
+        c=(b-32f)/1.8f;
+        System.out.println("user in put "+c+"C");
+    }
+    else if(a.equals("C")){
+        c=(b*1.8f)+32f;
+        System.out.println("user in put "+c+"F");
+    }
+    else{
+        System.out.println("user in put error!");
+    }
+}
+}
